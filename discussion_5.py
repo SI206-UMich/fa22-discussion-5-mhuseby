2f04a5e165dfa43354e4475b39249de6787b1ee3
mhuseby <mhuseby@umich.edu>
2022-09-28 18:26:00 -0700

    finished all but final test

--- discussion_5.py
+++ discussion_5.py
@@ -3,8 +3,8 @@ import unittest
 # Counts the number of a's in a sentence (e.g., a string)
 def count_a(sentence):
 	total = 0
-	for i in range(len(sentence) - 1):
-		if i == 'a':
+	for i in range(len(sentence)):
+		if sentence[i] == 'a':
 			total += 1
 	return total
 
@@ -38,15 +38,29 @@ class Warehouse:
 
 	# Adds an item to the warehouse	
 	def add_item(self, item):
-		pass
+		self.items.append(item)
+		
 
 	# Returns the item in the warehouse with the most stock		
 	def get_max_stock(self):
-		pass
+		max_stock = 0
+		max_stock_item = None
+		for item in self.items:
+			if item.stock >max_stock:
+				max_stock = item.stock
+				max_stock_item = item
+		return max_stock_item
+			
 	
 	# Returns the item in the warehouse with the highest price
 	def get_max_price(self):
-		pass	
+		max_price = 0
+		max_price_item = None
+		for item in self.items:
+			if item.price >max_price:
+				max_price = item.price
+				max_price_item = item
+		return max_price_item	
 
 
 
@@ -63,17 +77,31 @@ class TestAllMethods(unittest.TestCase):
 
 	## Check to see whether count_a works
 	def test_count_a(self):
-		pass
+		self.assertEqual(count_a("I feel like going to the apple store."), 1)
+		self.assertEqual(count_a("aaaaa"), 5)
+		self.assertEqual(count_a("None"), 0)
 
 
 	## Check to see whether you can add an item to the warehouse
 	def test_add_item(self):
-		pass
+		Target = Warehouse([self.item1, self.item2])
+		Target.add_item(self.item3)
+		self.assertEqual(Target.items, [self.item1, self.item2, self.item3])
 
 
 	## Check to see whether warehouse correctly returns the item with the most stock
 	def test_warehouse_max_stocks(self):
-		pass
+		Costco = Warehouse([self.item1, self.item2, self.item4])
+		mx_stock = Costco.get_max_stock()
+		self.assertEqual(mx_stock, self.item4)
+
+		Costco.add_item(self.item3)
+		mx_stock_new = Costco.get_max_stock()
+		self.assertEqual(mx_stock_new, self.item3)
+		# self.item1 = Item("Beer", 6, 20)
+		# self.item2 = Item("Cider", 5, 25)
+		# self.item3 = Item("Water", 1, 100)
+
 
 
 	# Check to see whether the warehouse correctly return the item with the highest price
